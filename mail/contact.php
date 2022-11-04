42ebb83aab2a20609d7dd0f977ac8a8d82bd19b0
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-03 22:08:33 -0300

    ok

--- mail/contact.php
+++ mail/contact.php
@@ -1,13 +1,15 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
+if(!empty($_POST['name']) || !empty($_POST['subject']) || !empty($_POST['message']) || filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  $name = strip_tags(htmlspecialchars($_POST['name']));
+  $email = strip_tags(htmlspecialchars($_POST['email']));
+  $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+  $message = strip_tags(htmlspecialchars($_POST['message']));
 }
 
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email = strip_tags(htmlspecialchars($_POST['email']));
-$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+// $name = strip_tags(htmlspecialchars($_POST['name']));
+// $email = strip_tags(htmlspecialchars($_POST['email']));
+// $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+// $message = strip_tags(htmlspecialchars($_POST['message']));
 
 $to = "info@example.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
